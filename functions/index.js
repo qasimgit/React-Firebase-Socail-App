275798fe5ac7838f8fc8cd824ae64f8c49ae1943
qasimgit <qasimpkkhi@gmail.com>
2020-12-05 23:03:07 +0500

    firebase or postman error

--- functions/index.js
+++ functions/index.js
@@ -9,3 +9,21 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
   console.log("Hello Qasim Form karachi");
   response.send("Hello Qasim Buddy Firebase!");
 });
+
+exports.getScreams = functions.https.onRequest((request, response) => {
+  return admin
+    .firestore()
+    .collection("screams")
+    .get()
+    .then((data) => {
+      let screams = [];
+      data.forEach((doc) => {
+        screams.push(doc.data());
+      });
+      return response.json(screams);
+    })
+    .catch((err) => {
+      console.error(err);
+      response.status(500).json({ error: err.code });
+    });
+});
